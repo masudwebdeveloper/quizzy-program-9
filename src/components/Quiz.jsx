4c946aac386e1358dc0e14df52761366bfd3fa2c
masudrana5 <abdullainfo30@gmail.com>
2022-10-11 20:05:46 +0600

    final score is added

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,41 +1,39 @@
-import React, { useState } from 'react';
 import Option from './Option';
 import { EyeIcon } from '@heroicons/react/24/solid'
 import { toast } from 'react-toastify'
-import { Link } from 'react-router-dom';
 
 
-
-
-const Quiz = ({ singlequestion }) => {
-   const { question, options } = singlequestion;
-   console.log(singlequestion);
+const Quiz = ({ singlequestion,score, setScore }) => {
+   const { question, options, correctAnswer } = singlequestion;
    const handlequiz = (quiz) => {
-      if (singlequestion.correctAnswer === quiz) {
-         toast.success('your answer is right', { autoClose: 500 })
+      const correctExist = options.find(existQuiz => existQuiz === quiz);
+      if (correctAnswer === correctExist) {
+         toast.success('your answer is right!', { autoClose: 500 })
+         setScore(score + 1);
       } else {
          toast.error('your answer is wrong!', { autoClose: 500 })
       }
+
+
    }
    const handleEye = () => {
-      const correctAnswer = singlequestion.correctAnswer;
       toast.success(correctAnswer, { autoClose: 5000 })
 
    }
    return (
-      <div className='bg-gray-100 m-4 w-1/2 mx-auto p-4 rounded-lg'>
+      <div>
+         {/* header */}
 
-         <div className='text-xl mb-3 text-center'>Quiz:
-            {
-               question === 'string' && question
-            }
-         </div>
-
-         <EyeIcon onClick={handleEye} className="h-6 w-6 text-blue-500" />
-         <div className='grid grid-cols-2 gap-2 shadow-lg pb-4 rounded-lg' >
-            {
-               options.map((option, index) => <Option key={index} option={option} handlequiz={handlequiz} />)
-            }
+         <div className='bg-gray-100 m-4 w-1/2 mx-auto p-4 rounded-[30px]'>
+            <h1 className='text-xl mb-3 text-center'>Quiz:
+               {question}
+            </h1>
+            <EyeIcon onClick={handleEye} className="h-6 w-6 text-blue-500" />
+            <div className='gap-4 shadow-lg pb-4 rounded-[25px]' >
+               {
+                  options.map((option, index) => <Option key={index} option={option} handlequiz={handlequiz} />)
+               }
+            </div>
          </div>
       </div>
    );
--- src/components/Topics.jsx
+++ src/components/Topics.jsx
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
 import Quiz from './Quiz';
 
 const Topics = () => {
+   const [score, setScore] = useState(0);
+   const [currentQuestion, setCurrentQuestion] = useState(0);
    const topics = useLoaderData();
    const { name, questions } = topics.data;
+   console.log(questions);
    return (
       <div>
-         <h1 className='text-4xl font-bold'>{name}</h1>
+         {/* header */}
+         <h1 className='text-4xl font-bold mb-5'>Quiz of {name}</h1>
+         <h2 className='text-3xl font-bold mb-5'> Your Final Score: { score} / { questions.length}</h2>
+         <h2 className='text-3xl font-bold'>Question {currentQuestion + 1} out of { questions.length}</h2>
          {
             questions.map(singlequestion => <Quiz
                key={singlequestion.id}
                singlequestion={singlequestion}
+               score={score}
+               setScore={setScore}
             />)
          }
          <button className='text-3xl underline mb-5 bg-green-500 hover:bg-green-700 duration-500 p-3 rounded-md'>
